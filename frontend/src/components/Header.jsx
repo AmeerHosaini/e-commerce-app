6c7ffedce2c9f7a60eb266ed82ea3a00010e15c5
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-09-26 22:53:15 +0430

    create the navbar

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart className="mb-1 mr-2" />
+                Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaUser className="mb-1 mr-2" />
+                Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
