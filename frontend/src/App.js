87c13b4901994da096a6928b1f9476bf5fa00443
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-09-26 23:51:38 +0430

    import Home component into App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 import { Container } from "react-bootstrap";
 
 const App = () => {
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Home />
         </Container>
       </main>
       <Footer />
