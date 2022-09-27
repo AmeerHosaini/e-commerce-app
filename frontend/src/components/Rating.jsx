1f2a577e67085decba13d4f1f575f221450ef575
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-09-27 22:42:23 +0430

    fix the Rating component

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -72,10 +72,10 @@ Rating.defaultProps = {
   color: "#f8e825",
 };
 
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
-};
+// Rating.propTypes = {
+//   value: PropTypes.number.isRequired,
+//   text: PropTypes.string.isRequired,
+//   color: PropTypes.string,
+// };
 
 export default Rating;
