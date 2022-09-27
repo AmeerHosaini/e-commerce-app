fe816dfed9af5b645b437888153365f81b071547
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-09-27 19:07:55 +0430

    implement the react router and create two pages; product, Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<Product />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
