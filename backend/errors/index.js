fdf9993ae16ee6b6b49bcf37bc3939f73fd6cf67
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-10-04 17:00:35 +0430

    create the index js file and group all the error files together

---
+++ backend/errors/index.js
@@ -0,0 +1,11 @@
+const BadRequest = require("./BadRequest");
+const CustomError = require("./CustomError");
+const NotFound = require("./NotFound");
+const UnAuthenticated = require("./UnAuthenticated");
+
+module.exports = {
+  BadRequest,
+  CustomError,
+  NotFound,
+  UnAuthenticated,
+};
