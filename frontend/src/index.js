2b154b620dda81e329fcb39a903e4281dc4c83e6
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-10-06 16:31:20 +0430

    import the Provider and store into index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./bootstrap.min.css";
 import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
