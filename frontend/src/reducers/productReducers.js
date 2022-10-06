9bbe4dea8b13e3733f6bd823d4beff758949f8de
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-10-06 16:41:41 +0430

    create productReducer.js

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,16 @@
+const productListReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case "PRODUCT_LIST_REQUEST":
+      return { loading: true, products: [] };
+    case "PRODUCT_LIST_SUCCESS":
+      return { loading: false, products: action.payload };
+    case "PRODUCT_LIST_FAIL":
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+// If it's successful, we'll send the data in the payload
+// If there's an error, we'll send the error in the payload
+// In order to use this reducer, we have to add it to the store.js
