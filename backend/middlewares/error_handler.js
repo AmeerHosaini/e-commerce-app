c6d381dc8df5986c93a3493fa606e2876d0a0210
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-10-31 14:12:20 +0430

    add more errors

--- backend/middlewares/error_handler.js
+++ backend/middlewares/error_handler.js
@@ -13,6 +13,20 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = StatusCodes.NOT_FOUND;
   }
 
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
