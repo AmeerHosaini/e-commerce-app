4ba951acf3ae2267292a17c6d33ef7f81939cdab
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-10-31 23:34:55 +0430

    create the login page

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import Loader from "../components/Loader";
+import { login } from "../actions/userActions";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, error, userInfo } = userLogin;
+
+  // We want to redirect
+  // We dont want to come to the login page if we are logged in
+  useEffect(() => {
+    // if user is not logged in, it will be null
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // Dispatch Login
+    dispatch(login(email, password));
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label className="mt-2">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button className="mt-3" type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          New Customer?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginPage;
