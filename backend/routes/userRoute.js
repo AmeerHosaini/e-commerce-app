7176deeeeba6c7dc41928ce91abe78dde3056fae
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-10-31 14:13:44 +0430

    create the route for registering a user

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middlewares/auth_middleware");
 
-const { authUser, getUserProfile } = require("../controllers/userController");
+const {
+  authUser,
+  getUserProfile,
+  registerUser,
+} = require("../controllers/userController");
 
+router.post("/", registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
