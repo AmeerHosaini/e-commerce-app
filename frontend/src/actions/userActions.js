ee10e00d3956d0147665e5048c14766cd2498fe8
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-01 15:45:33 +0430

    add logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,28 +3,25 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from "../constants/userConstant";
 
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: USER_LOGIN_REQUEST });
-
     // When we are sending data, we want to send in the headers a content type of application json
     const config = {
       headers: {
         "Content-Type": "application/json",
       },
     };
-
     // We will get id, name, isAdmin, token in data
     const { data } = await axios.post(
       "/api/users/login",
       { email, password },
       config
     );
-
     dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-
     localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
@@ -35,4 +32,10 @@ export const login = (email, password) => async (dispatch) => {
           : error.msg,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  // Remove the user from localstorage first
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
 };
