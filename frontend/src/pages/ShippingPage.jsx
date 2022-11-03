d1f64fe3ad8721398dcee57e854bbdaa92d7c29b
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-03 18:57:04 +0430

    create Shipping Page

---
+++ frontend/src/pages/ShippingPage.jsx
@@ -0,0 +1,8 @@
+const ShippingPage = () => {
+  return <div>Shipping</div>;
+};
+
+export default ShippingPage;
+
+// We want to save our shipping address by filling out a form and submit it
+// We want to have an action that adds the shipping address to the cart part of state and local storage
