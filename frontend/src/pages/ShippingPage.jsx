09f5bb46f1800a9c1cd74cb289699af76a967586
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-04 14:22:22 +0430

    add the functionality to submit data

--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -3,16 +3,25 @@ import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import { saveShippingAddress } from "../actions/cartAction";
 
 const ShippingPage = () => {
   const navigate = useNavigate();
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
+
+  const dispatch = useDispatch();
 
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate(`/payment`);
   };
 
   return (
