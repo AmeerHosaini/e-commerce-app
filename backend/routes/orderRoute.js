6b414a0b8c502c6f7814835fe48c687a6fd2a516
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-05 18:55:44 +0430

    create the route for /api/orders/:id

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { addOrderItems } = require("../controllers/orderController");
+const {
+  addOrderItems,
+  getOrderById,
+} = require("../controllers/orderController");
 const protect = require("../middlewares/auth_middleware");
 
 router.route("/").post(protect, addOrderItems);
+// This must always be at the bottom - if we set another route as /route, it will look at it as an id
+router.route("/:id").get(protect, getOrderById);
 
 module.exports = router;
