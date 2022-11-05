f278ebdbd934c090a332ed362dbc69c80dd9c4a8
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-05 19:13:21 +0430

    import getOrderDetailsReducer into store and combineReducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,10 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
-import { orderCreateReducer } from "./reducers/orderReducers";
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from "./reducers/orderReducers";
 
 // We must always add our reducers to the store
 
@@ -28,6 +31,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 // We can get our cartItems from here, or tokens.
