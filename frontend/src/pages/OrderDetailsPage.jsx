75c5038ff1588ef03a1939b202bbe2ffa0722bd4
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-05 20:21:19 +0430

    create OrderDetailsPage and add its functionalities

---
+++ frontend/src/pages/OrderDetailsPage.jsx
@@ -0,0 +1,148 @@
+import { useState, useEffect } from "react";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { getOrderDetails } from "../actions/orderActions";
+
+const OrderDetailsPage = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { loading, order, error } = orderDetails;
+
+  const addDecimals = (number) => {
+    return (Math.round(number * 100) / 100).toFixed(2);
+  };
+  // Calculate Prices
+  order.itemsPrice = addDecimals(
+    order.orderItems.reduce(
+      (acc, currentItem) => acc + currentItem.price * currentItem.quantity,
+      0
+    )
+  ); // 0 is the start of the accumulator
+
+  useEffect(() => {
+    dispatch(getOrderDetails(id));
+  }, [dispatch, id]);
+
+  // if it's loading, show loader. else if there is an error
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+              <p>
+                <strong>Name: </strong> {order.user.name}
+              </p>
+              <p>
+                <strong>Email: </strong>{" "}
+                <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+              </p>
+              <p>
+                <strong>Address: </strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city},{" "}
+                {order.shippingAddress.postalCode},{" "}
+                {order.shippingAddress.country}
+              </p>
+              {order.isDelivered ? (
+                <Message variant="success">
+                  Delivered {order.deliveredAt}
+                </Message>
+              ) : (
+                <Message variant="danger">Not delivered</Message>
+              )}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              <p>
+                <strong>Method: </strong>
+                {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="danger">Not paid</Message>
+              )}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {order.orderItems.length === 0 ? (
+                <Message>Your Order is Empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {order.orderItems.map((orderItem, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={orderItem.image}
+                            alt={orderItem.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/product/${orderItem.product}`}>
+                            {orderItem.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {orderItem.quantity} x ${orderItem.price} = $
+                          {orderItem.quantity * orderItem.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${order.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${order.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${order.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default OrderDetailsPage;
