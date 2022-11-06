3b50bf01f2dc2e6231f40fd41a8ef6cfbb2a559b
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-06 18:33:04 +0430

    create a route for updateToPaid controller

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require("../controllers/orderController");
 const protect = require("../middlewares/auth_middleware");
 
 router.route("/").post(protect, addOrderItems);
 // This must always be at the bottom - if we set another route as /route, it will look at it as an id
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").patch(protect, updateOrderToPaid);
 
 module.exports = router;
