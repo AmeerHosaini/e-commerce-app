d0796fba39c1350a12292a927f7362a74c823e06
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-06 20:37:13 +0430

    create a controller for paypal payment

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use("/api/products", productRoute);
 app.use("/api/users", userRoute);
 app.use("/api/orders", orderRoute);
 
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
+
 // Error Handler Middleware --- NOTE: This must be included below the routes and every other middleware
 app.use(errorHandlerMiddleware);
 
