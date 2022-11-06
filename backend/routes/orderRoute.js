ade3bd7a9235713f65a83b0baaac922246801cdd
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-06 22:15:48 +0430

    create a route for getLoggedInUser controller

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,10 +4,12 @@ const {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getLoggedInUserOrders,
 } = require("../controllers/orderController");
 const protect = require("../middlewares/auth_middleware");
 
 router.route("/").post(protect, addOrderItems);
+router.route("/myOrders").get(protect, getLoggedInUserOrders);
 // This must always be at the bottom - if we set another route as /route, it will look at it as an id
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").patch(protect, updateOrderToPaid);
