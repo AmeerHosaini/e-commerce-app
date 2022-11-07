da15d3b31cc3a055bf04b9aa1029c1d89749cd06
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-07 13:29:25 +0430

    create a route for /api/users to get the users for admin

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const protect = require("../middlewares/auth_middleware");
+const { protect, isAdmin } = require("../middlewares/auth_middleware");
 
 const {
   authUser,
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
 } = require("../controllers/userController");
 
-router.post("/", registerUser);
+router.route("/").post(registerUser).get(protect, isAdmin, getUsers);
 router.post("/login", authUser);
 router
   .route("/profile")
