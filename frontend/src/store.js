1c79648e942dbf54abce9f7cb15a0a382989348f
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-08 22:32:01 +0430

    create product create constants, reducer, add it to store, action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import {
   productListReducer,
   productDetailsReducer,
   productDeleteReducer,
+  productCreateReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -29,6 +30,7 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   // when we useSelect these, we will reference it by object key
