966485dbb4b1c47a6bd152e6bcbe02854151c875
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-10 20:22:32 +0430

    create the orderListPage and add the details functionality

---
+++ frontend/src/pages/OrderListPage.jsx
@@ -0,0 +1,90 @@
+import { useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listOrders } from "../actions/orderActions";
+
+const OrderListPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const orderList = useSelector((state) => state.orderList);
+  const { loading, error, orders } = orderList;
+
+  /* Security Check - When we log out as admin, we will see the users list and if we reload, we get token null
+    2. When we login as a user and manually hit /admin/userlist route, we will get a not an admin custom error
+    we don't want even want the user to access that page
+  */
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    // if logged in and the logged in account is the admin
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listOrders());
+    } else {
+      navigate("/login");
+    }
+    // When admin deletes a user, we want the page to refresh and show the remaining users
+  }, [dispatch, navigate, userInfo]);
+
+  return (
+    <>
+      <h1>Orders</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>USER</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.user && order.user.name}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>${order.totalPrice}</td>
+                <td>
+                  {order.isPaid ? (
+                    order.paidAt.substring(0, 10)
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "red" }}></i>
+                  )}
+                </td>
+                <td>
+                  {order.isDelivered ? (
+                    order.deliveredAt.substring(0, 10)
+                  ) : (
+                    <i className="fas fa-times" style={{ color: "red" }}></i>
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/order/${order._id}`}>
+                    <Button variant="primary" className="btn-sm">
+                      Details
+                    </Button>
+                  </LinkContainer>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default OrderListPage;
