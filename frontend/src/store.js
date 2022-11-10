7ccd126268df5cbb38182201c41aed0226d096df
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-10 22:29:41 +0430

    add the orderDeliverReducer to the store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -22,6 +22,7 @@ import {
   orderCreateReducer,
   orderDetailsReducer,
   orderPayReducer,
+  orderDeliverReducer,
   orderListMyReducer,
   orderListReducer,
 } from "./reducers/orderReducers";
@@ -48,6 +49,7 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderDeliver: orderDeliverReducer,
   orderListMy: orderListMyReducer,
   orderList: orderListReducer,
 });
