9a4a3762f58ad7f4b38dd066c456a7bb6bc68067
Ameer Hosaini <ameer.hosaini15@gmail.com>
2022-11-11 23:51:23 +0430

    bug fix - inline and place holder

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -17,12 +17,12 @@ const SearchBox = () => {
   };
 
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} className="d-flex">
       <Form.Control
         type="text"
         name="q"
         onChange={(e) => setKeyword(e.target.value)}
-        placeHolder="Search Products..."
+        placeholder="Search Products..."
         className="mr-sm-2 ml-sm-5"
       ></Form.Control>
       <Button type="submit" variant="outline-success" className="p-2">
